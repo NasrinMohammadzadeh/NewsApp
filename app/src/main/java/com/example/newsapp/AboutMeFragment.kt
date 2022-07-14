f8ae31874879986cc8d55bbd64c78e3e8a03952b
NasrinMohammadzadeh <nasrin4672@gmail.com>
2022-07-14 13:52:08 +0430

    bottom navigation navigate bug fixed

--- app/src/main/java/com/example/newsapp/AboutMeFragment.kt
+++ app/src/main/java/com/example/newsapp/AboutMeFragment.kt
@@ -1,6 +1,31 @@
 package com.example.newsapp
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.example.newsapp.databinding.FragmentAboutMeBinding
+import com.example.newsapp.util.base.BaseFragment
 
-class AboutMeFragment : Fragment() {
+class AboutMeFragment : BaseFragment<FragmentAboutMeBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_about_me
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+        binding.apply {
+
+//            toolbar.root.setOnClickListener {
+//                onScrollToTop()
+//            }
+//
+//            toolbar.ivBack.setOnClickListener {
+//                mainHelper.goBack()
+//            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/newsapp/NewsListFragment.kt
+++ app/src/main/java/com/example/newsapp/NewsListFragment.kt
@@ -1,6 +1,31 @@
 package com.example.newsapp
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.example.newsapp.databinding.FragmentNewsListBinding
+import com.example.newsapp.util.base.BaseFragment
 
-class NewsListFragment : Fragment() {
+class NewsListFragment : BaseFragment<FragmentNewsListBinding>() {
+    override val layoutId: Int
+        get() = R.layout.fragment_news_list
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+
+        binding.apply {
+
+//            toolbar.root.setOnClickListener {
+//                onScrollToTop()
+//            }
+//
+//            toolbar.ivBack.setOnClickListener {
+//                mainHelper.goBack()
+//            }
+        }
+    }
+
 }
